8ad5bb92551cfa6fe0cb6fd679c714fd59821ad6
excniesNIED <hxcn@cnies.org>
2025-03-04 09:21:24 +0800

    fix: Site_name var to NAO Chat

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -107,11 +107,11 @@ for source in log_sources:
 log.setLevel(SRC_LOG_LEVELS["CONFIG"])
 
 
-WEBUI_NAME = os.environ.get("WEBUI_NAME", "Open WebUI")
-if WEBUI_NAME != "Open WebUI":
-    WEBUI_NAME += " (Open WebUI)"
+WEBUI_NAME = os.environ.get("WEBUI_NAME", "NAO Chat")
+if WEBUI_NAME != "NAO Chat":
+    WEBUI_NAME += " (NAO Chat)"
 
-WEBUI_FAVICON_URL = "https://openwebui.com/favicon.png"
+WEBUI_FAVICON_URL = "https://gastigado.cnies.org/d/nao/favicon.png?sign=M1lDDrOct7BJRDEV6TUNY82JZC7EsPhkVjFT-O7FLe8=:0"
 
 TRUSTED_SIGNATURE_KEY = os.environ.get("TRUSTED_SIGNATURE_KEY", "")
 
